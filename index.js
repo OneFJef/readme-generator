797733219da402ff4bbfcf72ac695d21f21fd683
Jef Mitchell <jef@project87.net>
2021-08-19 21:03:23 -0400

    Update Javascript - (index.js) Created initial prompt questions.

--- index.js
+++ index.js
@@ -1,7 +1,46 @@
 // TODO: Include packages needed for this application
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+      name: "title",
+      message: "Project Title:",
+    },
+    {
+      name: "description",
+      message: "Project Description:",
+    },
+    {
+      name: "installation",
+      message: "Installation Instructions:",
+    },
+    {
+      name: "usage",
+      message: "Usage Instructions:",
+    },
+    {
+      name: "contribution",
+      message: "Contribution Guidelines:",
+    },
+    {
+      name: "test",
+      message: "Test Instructions:",
+    },
+    {
+      name: "license",
+      message: "Liscense:",
+      type: "list",
+      choices: ["MIT", "Apache 2.0", "GPL 3.0", "BSD 3", "None"],
+    },
+    {
+      name: "github",
+      message: "Github Username:",
+    },
+    {
+      name: "email",
+      message: "E-Mail Address:",
+    },
+  ];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
