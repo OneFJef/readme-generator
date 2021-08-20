62e9adad2c19464139fb15ae2b6dab2398bf195a
Jef Mitchell <jef@project87.net>
2021-08-19 21:45:22 -0400

    Update Javascript - (index.js) Formated code.

--- index.js
+++ index.js
@@ -5,61 +5,64 @@ const generateMarkdown = require("./utils/generateMarkdown");
 
 // TODO: Create an array of questions for user input
 const questions = [
-    {
-      name: "title",
-      message: "Project Title:",
-    },
-    {
-      name: "description",
-      message: "Project Description:",
-    },
-    {
-      name: "installation",
-      message: "Installation Instructions:",
-    },
-    {
-      name: "usage",
-      message: "Usage Instructions:",
-    },
-    {
-      name: "contribution",
-      message: "Contribution Guidelines:",
-    },
-    {
-      name: "test",
-      message: "Test Instructions:",
-    },
-    {
-      name: "license",
-      message: "Liscense:",
-      type: "list",
-      choices: ["MIT", "Apache 2.0", "GPL 3.0", "BSD 3", "None"],
-    },
-    {
-      name: "github",
-      message: "Github Username:",
-    },
-    {
-      name: "email",
-      message: "E-Mail Address:",
-    },
-  ];
+  {
+    name: "title",
+    message: "Project Title:",
+  },
+  {
+    name: "description",
+    message: "Project Description:",
+  },
+  {
+    name: "installation",
+    message: "Installation Instructions:",
+  },
+  {
+    name: "usage",
+    message: "Usage Instructions:",
+  },
+  {
+    name: "contribution",
+    message: "Contribution Guidelines:",
+  },
+  {
+    name: "test",
+    message: "Test Instructions:",
+  },
+  {
+    name: "license",
+    message: "Liscense:",
+    type: "list",
+    choices: ["MIT", "Apache 2.0", "GPL 3.0", "BSD 3", "None"],
+  },
+  {
+    name: "github",
+    message: "Github Username:",
+  },
+  {
+    name: "email",
+    message: "E-Mail Address:",
+  },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
-    fs.writeFile(fileName, data, (err) =>
-      err
-        ? console.log(err)
-        : console.log("Success: Your file has been generated.")
-    );
-  }
+  fs.writeFile(fileName, data, (err) =>
+    err
+      ? console.log(err)
+      : console.log("Success: Your file has been generated.")
+  );
+}
 
 // TODO: Create a function to initialize app
 function init() {
-    inquirer.prompt(questions).then((answers) => {
-      writeToFile(`${answers.title}_README.md`, generateMarkdown.generateMarkdown(answers));
-    });
-  }
+  inquirer.prompt(questions).then((answers) => {
+    writeToFile(
+      `${answers.title}_README.md`,
+      generateMarkdown.generateMarkdown(answers)
+    );
+  });
+}
 
 // Function call to initialize app
 init();
