d3184d1c7e0bd958e4440802661ef4abab17510d
Jef Mitchell <jef@project87.net>
2021-08-19 21:10:26 -0400

    Update Javascript - (generateMarkdown.js) renderLicenseBadge function created.

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,19 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+    switch (license) {
+      case "MIT":
+        return "![License](https://img.shields.io/badge/license-MIT-blue)\n";
+      case "Apache 2.0":
+        return "![License](https://img.shields.io/badge/license-Apache%202.0-blue)\n";
+      case "GPL 3.0":
+        return "![License](https://img.shields.io/badge/license-GPL%203.0-blue)\n";
+      case "BSD 3":
+        return "![License](https://img.shields.io/badge/license-BSD%203-blue)\n";
+      default:
+        break;
+    }
+  }
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
